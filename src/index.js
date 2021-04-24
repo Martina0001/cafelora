3770fef7f4949aa59e0d5344b6d9189c37846575
Martina Hejná <m.sejkova@gmail.com>
2021-04-24 12:41:32 +0200

    navigace reakce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+const navigation = document.querySelector('nav');
+const allItems = document.querySelectorAll('.nav_link');
+const nav = document.querySelectorAll('#nav-btn');
+
+nav.addEventListener('click', (e) => {
+  navigation.classList.toggle('nav');
+});
+
+allItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    navigation.classList.toggle('nav-closed');
+  });
+});
