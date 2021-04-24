63f334d34e280799c82a64a07414e14ded1ee095
Martina Hejná <m.sejkova@gmail.com>
2021-04-24 15:49:41 +0200

    seznam ingrediencí

--- src/index.js
+++ src/index.js
@@ -15,13 +15,66 @@ allItems.forEach((item) => {
   });
 });
 
-/***objednávka ***/
+//*const inputElm = document.querySelector('#form-input');
+//const buttonElm = document.querySelector('#form-button');
+//const alertElm = document.querySelector('#form-alert');
 
+//buttonElm.addEventListener('click', (event) => {
+// alertElm.textContent = `Uživatel ${inputElm.value} už existuje.`;
+// event.target.textContent = 'Zkusit znova';
+//});
+
+//
+
+/***objednávka ***/
 const order = document.querySelector('.order-btn');
 const selectedOrder = document.querySelector('.drink__cup');
+const btnWhenClick = e.target;
 
 order.addEventListener('click', (e) => {
-  const buttonAfterClick = e.target;
-  selectedOrder.classList.toggle('drink__cup--selected');
-  order.textContent = 'Zrušit';
+  if (ordered === false) {
+    btnWhenClickElm.classList.add('drink__cup--selected');
+    btnWhenClickElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    btnWhenClickElm.classList.remove('drink__cup--selected');
+    btnWhenClickElm.textContent = 'Objednat';
+    ordered = false;
+  }
 });
+
+////****5-ingredience jako komponenty */
+const Layer = (text) => {
+  return `<h1>${text}</h1>`;
+};
+const appElm = document.querySelector('#app');
+appElm.innerHTML = {
+  color: '#feeeca',
+  label: 'mléčná pěna',
+};
+
+/****6-seznam ingrediencí */
+
+const layers = [
+  {
+    color: '#feeeca',
+    label: 'mléčná pěna',
+  },
+  {
+    color: '#fed7b0',
+    label: 'teplé mléko',
+  },
+  {
+    color: '#613916',
+    label: 'espresso',
+  },
+];
+
+const appElm = document.querySelector('.drink__info');
+
+layers.forEach((layer) => {
+  appElm.innerHTML + Layer(layer);
+  appElm.innerHTML = Layer(layer);
+});
+
+//*7-
